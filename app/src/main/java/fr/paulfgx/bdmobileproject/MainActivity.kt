acec80e6f8b348e17f6f7eb7e8fbb9b92995d619
PaulFgx <pfourgoux@live.fr>
2020-04-08 09:57:22 +0200

    read/write to firebase ok

--- app/src/main/java/fr/paulfgx/bdmobileproject/MainActivity.kt
+++ app/src/main/java/fr/paulfgx/bdmobileproject/MainActivity.kt
@@ -2,10 +2,19 @@ package fr.paulfgx.bdmobileproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         val myRef = database.getReference("message")
 
         myRef.setValue("Hello, World!")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
